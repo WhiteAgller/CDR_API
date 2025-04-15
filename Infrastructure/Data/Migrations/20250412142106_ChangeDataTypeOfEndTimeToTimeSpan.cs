2155898f4457b3d2d0f3db668d38cc4f481d7d66
Jan Straka <agller738@gmail.com>
2025-04-15 20:18:00 +0200

    fix: change datatype of EndTime to TimeSpan

---
+++ Infrastructure/Data/Migrations/20250412142106_ChangeDataTypeOfEndTimeToTimeSpan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeDataTypeOfEndTimeToTimeSpan : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "EndTime",
+                table: "CallRecords",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndTime",
+                table: "CallRecords",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+        }
+    }
+}
